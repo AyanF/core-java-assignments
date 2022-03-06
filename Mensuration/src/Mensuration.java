004b4df8ac5db0a1d784cb714d62c85b931d5c0a
AyanF <ayanfarooqui211@gmail.com>
2022-03-06 14:01:35 +0530

    Displaying shape menu with JOptionPane

--- Mensuration/src/Mensuration.java
+++ Mensuration/src/Mensuration.java
@@ -1,16 +1,22 @@
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Mensuration {
 	
 	Scanner sc = new Scanner(System.in);
 	
-	static void displayMenu(){
+	static int displayMenu(){
 		
-		System.out.println("Select a shape \n 1.Circle \n 2.Square \n 3.Rectangle \n 4.Triangle"+ 
+		String choiceStr = JOptionPane.showInputDialog(null,"Select a shape \n 1.Circle \n 2.Square \n 3.Rectangle \n 4.Triangle"+ 
                 		   "\n 5.Parallelogram \n 5.Semi-Circle \n 6.Rhombus \n 7.Trapezium" +
 		           		   "\n 8.Sphere \n 9.Cube \n 10.Cuboid \n 11.Cone \n 12.Cylinder");
-		}
+		
+		int choice = Integer.parseInt(choiceStr);
+		
+		return choice;
+		
+	}
 
 	public static void main(String[] args) {
 		
