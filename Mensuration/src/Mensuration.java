ef6fd0626bc900f7b60c54424016a0591551e930
AyanF <ayanfarooqui211@gmail.com>
2022-03-06 13:51:36 +0530

    Display list of shapes on console

---
+++ Mensuration/src/Mensuration.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Mensuration {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	static void displayMenu(){
+		
+		System.out.println("Select a shape \n 1.Circle \n 2.Square \n 3.Rectangle \n 4.Triangle"+ 
+                		   "\n 5.Parallelogram \n 5.Semi-Circle \n 6.Rhombus \n 7.Trapezium" +
+		           		   "\n 8.Sphere \n 9.Cube \n 10.Cuboid \n 11.Cone \n 12.Cylinder");
+		}
+
+	public static void main(String[] args) {
+		
+		
+		displayMenu();
+		
+		
+	}
+		
+		 
+	}
+
+
