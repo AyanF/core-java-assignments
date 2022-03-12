b625615481240c3a78163f1c8460ce4f4cf22523
AyanF <ayanfarooqui211@gmail.com>
2022-03-12 16:03:46 +0530

    Overdraw safe implemented

---
+++ Account/src/AccountOverdrawDemoSafe.java
@@ -0,0 +1,33 @@
+public class AccountOverdrawDemoSafe implements Runnable {              
+    
+   Account acc = new Account();
+    
+    public static void main(String[] args) {
+        
+        AccountOverdrawDemoSafe overDrawSafe = new AccountOverdrawDemoSafe();
+        
+        Thread firstPerson = new Thread(overDrawSafe);
+        Thread secondPerson = new Thread(overDrawSafe);
+        
+        firstPerson.setName("first person");
+        firstPerson.start();
+        
+        secondPerson.setName("second person");
+        secondPerson.start();
+
+    }
+    
+    @Override
+    public void run() {
+        
+        synchronized (acc) {
+         
+            for(int i=0; i<4; i++) {
+            
+                acc.withdraw(100);
+            }
+        }
+        
+    }
+
+}
